126e6879912e7e1d9358d42fb332c77bc9d73183
Lawitadelimon <lau72abrvjua@gmail.com>
2025-10-16 11:44:30 -0600

    login1

--- sw.js
+++ sw.js
@@ -47,7 +47,7 @@ self.addEventListener('activate', event => {
 self.addEventListener('fetch', event => {
     event.respondWith(
         caches.match(event.request).then(response => {
-            return response || fetch(event.request).catch(() => caches.match(`${BASE_PATH}offline.html`));
+            return response || fetch(event.request).catch(() => caches.match(`${BASE_PATH}login.html`));
         })
     );
 });
